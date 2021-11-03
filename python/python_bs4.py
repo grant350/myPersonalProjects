e284b653dd196d11d687242a0f48412128efb585
Grant Mitchell <soundbreakr1@gmail.com>
2021-11-02 17:24:42 -0700

    moved python

--- python/python_bs4.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup as bs4
-import requests;
-url = requests.get("http://www.clothesabyss.com")
-print(url.status_code)
-
-soup = bs4(url.content, "html.parser")
-
-print(soup.prettify())
